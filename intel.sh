11ffe8868d3f0c3e2d43dce3f39fa56412b54dc8
Janmajaya Mall <janmajaya.choithram@gmail.com>
2023-03-01 09:32:53 +0530

    add +nightly to scripts

--- intel.sh
+++ intel.sh
@@ -1,4 +1,4 @@
 export RUSTFLAGS="-O -C target-feature=+avx512ifma"
 export RAYON_NUM_THREADS=1
-cargo update
-cargo run --release
+cargo +nightly update
+cargo +nightly run --release
--- mac.sh
+++ mac.sh
@@ -1,3 +1,3 @@
-cargo update
 export RAYON_NUM_THREADS=1
-cargo run --release
+cargo +nightly update
+cargo +nightly run --release
